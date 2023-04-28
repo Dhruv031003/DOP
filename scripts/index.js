d5d54a27008b75acfbee1cf2ed93422a0417795e
Dhruv031003 <dhruvdorbi03@gmail.com>
2023-04-28 23:08:32 +0530

    added landing container and changing text

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+var quotes = ['DESTINATION OF PEACE','FIND TRANQUILITY WITHIN','JOURNEY TO CALMNESS', 'EMBRACE THE STILLNESS'],
+    currentStep = 0,
+    textElement = document.querySelector('.landing-text'),
+    oldWord = '';
+
+setTimeout(changeWord, 500);
+
+function changeWord() {
+  oldWord = textElement.innerHTML;
+
+  if (oldWord.length < 1) {
+    if (currentStep !== quotes.length-1){
+          currentStep ++;
+    }else {
+      currentStep = 0;
+    }
+    addNextWord();
+  } else {
+    setTimeout(deleteWord, 500);
+  }
+  
+};
+
+function deleteWord() {
+  var currentWord = textElement.innerHTML,
+      currentLength = currentWord.length;
+
+  if (currentLength < 1) {
+    changeWord();
+    return;
+  }
+  textElement.innerHTML = currentWord.substring(0, currentLength - 1);
+  setTimeout(deleteWord, 200);
+}
+
+function addNextWord() {
+  var currentWord = textElement.innerHTML,
+      currentLength = currentWord.length,
+      nextWord = quotes[currentStep],
+      nextWordLength = nextWord.length;
+
+  if (currentLength === nextWordLength) {
+    changeWord();
+    return;
+  }
+  textElement.innerHTML = nextWord.substring(0, currentLength + 1); 
+  setTimeout(addNextWord, 200);
+  
+  
+}
+
