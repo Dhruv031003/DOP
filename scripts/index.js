4d0486be030df20f8686750d49308ebf4921ca33
Dhruv031003 <dhruvdorbi03@gmail.com>
2023-05-14 22:17:18 +0530

    contact and footer left

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 //NAVBAR HAMBURGER MENU 
 let NavAppear = () => {
-    let k = document.getElementById('middle').style;
-    if (k.display === "flex") {
-      k.display = "none";
-    }
-    else {
-      k.display = "flex";
-    }
+  let k = document.getElementById('middle').style;
+  if (k.display === "flex") {
+    k.display = "none";
   }
+  else {
+    k.display = "flex";
+  }
+}
 //**************
 
 //LANDING CONTAINER CHANGING TEXT
-var quotes = ['FIND TRANQUILITY WITHIN','JOURNEY TO CALMNESS', 'EMBRACE THE STILLNESS'],
-    currentStep = 0,
-    textElement = document.querySelector('.changing-text'),
-    oldWord = '';
+var quotes = ['FIND TRANQUILITY WITHIN', 'JOURNEY TO CALMNESS', 'EMBRACE THE STILLNESS'],
+  currentStep = 0,
+  textElement = document.querySelector('.changing-text'),
+  oldWord = '';
 
 setTimeout(changeWord, 500);
 
@@ -22,21 +22,21 @@ function changeWord() {
   oldWord = textElement.innerHTML;
 
   if (oldWord.length < 1) {
-    if (currentStep !== quotes.length-1){
-          currentStep ++;
-    }else {
+    if (currentStep !== quotes.length - 1) {
+      currentStep++;
+    } else {
       currentStep = 0;
     }
     addNextWord();
   } else {
     setTimeout(deleteWord, 500);
   }
-  
+
 };
 
 function deleteWord() {
   var currentWord = textElement.innerHTML,
-      currentLength = currentWord.length;
+    currentLength = currentWord.length;
 
   if (currentLength < 1) {
     changeWord();
@@ -48,17 +48,61 @@ function deleteWord() {
 
 function addNextWord() {
   var currentWord = textElement.innerHTML,
-      currentLength = currentWord.length,
-      nextWord = quotes[currentStep],
-      nextWordLength = nextWord.length;
+    currentLength = currentWord.length,
+    nextWord = quotes[currentStep],
+    nextWordLength = nextWord.length;
 
   if (currentLength === nextWordLength) {
     changeWord();
     return;
   }
-  textElement.innerHTML = nextWord.substring(0, currentLength + 1); 
+  textElement.innerHTML = nextWord.substring(0, currentLength + 1);
   setTimeout(addNextWord, 200);
-  
+
 }
 //***************
 
+//Review Slider
+const reviews = document.querySelectorAll(".review");
+let currentReview = 0;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+for (let i = 0; i < reviews.length; i++) {
+  reviews[currentReview].style.display = "block";
+}
+//Previous Review Function
+function prevReview() {
+  for (let i = 0; i < reviews.length; i++) {
+    reviews[currentReview].style.display = "none";
+  }
+  //currentReview Update
+  currentReview -= 1;
+  if (currentReview === -1) {
+    currentReview = reviews.length - 1;
+  }
+
+  for (let i = 0; i < reviews.length; i++) {
+    reviews[currentReview].style.display = "block";
+  }
+}
+//Next Review Function
+function nextReview() {
+  for (let i = 0; i < reviews.length; i++) {
+    reviews[currentReview].style.display = "none";
+  }
+  //currentReview Update
+  currentReview += 1;
+  if (currentReview === reviews.length) {
+    currentReview = 0;
+  }
+
+  for (let i = 0; i < reviews.length; i++) {
+    reviews[currentReview].style.display = "block";
+  }
+}
+
+prev.addEventListener("click", prevReview);
+next.addEventListener("click", nextReview);
+
+////////////////////////
